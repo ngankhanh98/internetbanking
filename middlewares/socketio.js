89dd9235363fcab9cb422e0f3f5196afc3098d4e
ngankhanh98 <ngankhanh98@gmail.com>
2020-07-09 14:20:04 +0700

    ♻ change socketio structurefile

--- middlewares/socketio.js
+++ middlewares/socketio.js
@@ -1,1 +1,21 @@
-const io = require('socket.io');
+const io = require('socket.io')();
+let socketAPI = {};
+
+const connectionModel = require('../models/connection.model');
+
+io.on('connection', (socket) => {
+
+    console.log('New connection')
+
+    socket.on('join', ({ username }) => {
+        console.log('username >>', username)
+
+    })
+
+    socket.on('disconnect', () => {
+        console.log('Kill connection')
+    })
+})
+
+socketAPI.io = io;
+module.exports = socketAPI;
--- models/connection.model.js
+++ models/connection.model.js
@@ -1,3 +1,26 @@
+const users = []
 module.exports = {
-    
+    addConnection: ({ id, name, room }) => {
+        name = name.trim().toLowerCase();
+        room = room.trim().toLowerCase();
+
+        const existingConnection = users.find((user) => user.room === room && user.name === name);
+
+        if (!name || !room) return { error: 'Connection and room are required.' };
+        if (existingConnection) return { error: 'Connection is taken.' };
+
+        const user = { id, name, room };
+
+        users.push(user);
+        console.log('users :>> ', users);
+        return { user };
+    },
+
+    removeConnection: (id) => {
+        const index = users.findIndex((user) => user.id === id);
+
+        if (index !== -1) return users.splice(index, 1)[0];
+    },
+    getConnection: (id) => users.find((user) => user.id === id),
+    getConnectionsInRoom: (room) => users.filter((user) => user.room === room)
 };
