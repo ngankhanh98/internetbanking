8f9ab86850edb2b6a1e052f2c27af516ec225fe2
ngankhanh98 <ngankhanh98@gmail.com>
2020-07-14 21:58:20 +0700

    🗑Delete comment

--- utils/db.js
+++ utils/db.js
@@ -1,52 +1,8 @@
-// const mysql = require("mysql");
-// const { promisify } = require("util");
-// const { _mysql } = require("../config/default.json");
-
-// const pool = mysql.createPool(_mysql);
-// const pool_query = promisify(pool.query).bind(pool);
-
-// module.exports = {
-//   load: (sql) => pool_query(sql),
-//   add: (entity, table) => pool_query(`insert into ${table} set ?`, entity),
-//   del: (condition, table) =>
-//     pool_query(`delete from ${table} where ?`, condition),
-//   update: (entity, condition, table) =>
-//     pool_query(`update ${table} set ? where ?`, [entity, condition]),
-// };
-
-
 const mysql = require('mysql');
 // const util = require('util');
 const { _mysql } = require("../config/default.json");
 
-// const connection = mysql.createConnection(_mysql);
-
 const createConnection = () => mysql.createConnection(_mysql);
-
-// const query = util.promisify(connection.query).bind(connection);
-// const end = util.promisify(connection.end).bind(connection);
-
-
-// module.exports = {
-//   load: async (sql) => {
-//     await query(sql)
-//     end()
-//   },
-//   add: async (entity, table) => {
-//     await query(`insert into ${table} set ?`, entity)
-//     end()
-//   },
-//   del: async (condition, table) => {
-//     await query(`delete from ${table} where ?`, condition)
-//     end()
-
-//   },
-//   update: async (entity, condition, table) => {
-//     await query(`update ${table} set ? where ?`, [entity, condition])
-//     end()
-
-//   }
-// };
 
 
 module.exports = {
